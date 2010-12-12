1f8e9ec078d34ddc28a88ccc9a1c6336da2237f7
Colin Sullivan <colinsul@gmail.com>
2010-12-11 22:35:36 -0500

    Documentation update.

---
+++ updateDocumentation.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+# The path to your jsdoc-toolkit folder (no ending slash)
+JSTOOLKIT_ROOT='/opt/local/bin/jsdoc-toolkit'
+
+
+
+# Get new client side code
+echo 'Updating client side code'
+git checkout master concertapp/static/js/
+# Remove vendor client side code (we dont need to document any)
+rm -R concertapp/static/js/lib/vendor/
+
+echo 'Generating client side documentation'
+java -jar $JSTOOLKIT_ROOT/jsrun.jar $JSTOOLKIT_ROOT/app/run.js -r -d=./clientside concertapp/static/js/ -t=$JSTOOLKIT_ROOT/templates/jsdoc/
+
+echo 'Adding clientside documentation to git'
+git add clientside/
+
+echo 'Committing'
+git commit -a -m 'Documentation update.'
