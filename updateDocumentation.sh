f2b51b4ba1f1edfa7c3b9ddd5c5bb0e1dd9151d8
Colin Sullivan <colinsul@gmail.com>
2010-12-11 22:39:31 -0500

    Comments on documentation script.  Could be more robus.

--- updateDocumentation.sh
+++ updateDocumentation.sh
@@ -11,14 +11,16 @@ git checkout master concertapp/static/js/
 # Remove vendor client side code (we dont need to document any)
 rm -R concertapp/static/js/lib/vendor/
 
+# Generate the client-side documentation
 echo 'Generating client side documentation'
 java -jar $JSTOOLKIT_ROOT/jsrun.jar $JSTOOLKIT_ROOT/app/run.js -r -d=./clientside concertapp/static/js/ -t=$JSTOOLKIT_ROOT/templates/jsdoc/
 
+# Commit changes
 echo 'Adding clientside documentation to git'
 git add clientside/
-
 echo 'Committing'
 git commit -a -m 'Documentation update.'
 
+# Push
 echo 'Pushing'
 git push origin gh-pages
